732ab5a78c028347d1b050c09d3e709596d119f3
Saipronoti <saipronotimukherjee27@gmail.com>
2024-02-21 12:33:53 +0530

    repository creation

---
+++ src/main/java/com/maersk/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.maersk.repository;
+
+import com.maersk.model.Booking;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookingRepository extends MongoRepository<Booking,String> {
+}
