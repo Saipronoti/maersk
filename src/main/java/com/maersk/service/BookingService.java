efad6dc5da446f0fa0a1e93ce74527d199c4515a
Saipronoti <saipronotimukherjee27@gmail.com>
2024-02-21 13:17:13 +0530

    repository method to retrieve booking

--- src/main/java/com/maersk/service/BookingService.java
+++ src/main/java/com/maersk/service/BookingService.java
@@ -5,6 +5,7 @@ import com.maersk.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,5 +16,13 @@ public class BookingService {
     public Booking saveBooking(Booking booking){
         booking.setBookingId(UUID.randomUUID().toString().split("-")[0]);
         return repository.save(booking);
+    }
+
+    public List<Booking> getAllBookings(){
+        return repository.findAll();
+    }
+
+    public Booking getBookingByBookingId(String bookingId){
+        return repository.findById(bookingId).get();
     }
 }
