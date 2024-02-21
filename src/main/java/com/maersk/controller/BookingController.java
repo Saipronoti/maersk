713e2ed306abfad547da184efa923a82fd3cf342
Saipronoti <saipronotimukherjee27@gmail.com>
2024-02-21 17:22:36 +0530

    controller for creating booking

--- src/main/java/com/maersk/controller/BookingController.java
+++ src/main/java/com/maersk/controller/BookingController.java
@@ -1,9 +1,19 @@
 package com.maersk.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.maersk.model.Booking;
+import com.maersk.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
+    @Autowired
+    private BookingService service;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Booking createBooking(@RequestBody Booking booking){
+        return service.saveBooking(booking);
+    }
 }
