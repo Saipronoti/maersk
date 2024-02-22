1f1c5a066c7a5ce55347ddb2958e65bb33f74e43
Saipronoti <saipronotimukherjee27@gmail.com>
2024-02-22 11:15:24 +0530

    created both post apis

--- src/main/java/com/maersk/controller/BookingController.java
+++ src/main/java/com/maersk/controller/BookingController.java
@@ -9,13 +9,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("api/bookings")
 public class BookingController {
     @Autowired
     private BookingService service;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping("/createBooking")
     public Booking createBooking(@RequestBody Booking booking){
+        return service.saveBooking(booking);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping("/checkAvailable")
+    public Booking checkAvailability(@RequestBody Booking booking){
         return service.saveBooking(booking);
     }
     @GetMapping
